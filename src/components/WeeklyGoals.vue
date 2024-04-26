13e48703c852272495896310930ab94de027e382
Saitarunreddy77 <167725144+Saitarunreddy77@users.noreply.github.com>
2024-04-26 19:23:15 -0400

    WeeklyGoals.vue

--- src/components/WeeklyGoals.vue
+++ src/components/WeeklyGoals.vue
@@ -1,6 +1,12 @@
 <template>
+  <div class="container">
+    <div class="logo">
+        <img src="https://static.vecteezy.com/system/resources/previews/021/425/348/non_2x/a-logo-for-a-health-and-fitness-company-free-vector.jpg" alt="Company Logo">
+    </div>
+    <h1>Weekly Fitness Goals</h1>
+</div>
+
     <div class="weekly-goals">
-      <h1>Weekly Fitness Goals</h1>
       <div v-for="(dayData, day) in weeklyData" :key="day">
         <div class="day-data">
           <h2>{{ day }}</h2>
@@ -25,23 +31,44 @@
   </script>
   
   <style scoped>
+  .container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+.logo {
+    max-width: 70px; 
+    text-align: center;
+}
+
+.logo img {
+    width: 100%;
+}
+
+h1 {
+    font-size: 24px;
+    margin-top: 10px; 
+}
+
   
   .weekly-goals {
-    flex: 1;
-    padding: 20px;
-    background-color: #f0f0f0;
+      flex: 1;
+      padding: 20px;
+      background-color: #f0f0f0;
   }
-
+  
   .weekly-goals h1 {
-  margin-bottom: 20px;
-  text-align: center;
-}
+      margin-bottom: 20px;
+      text-align: center;
+  }
   
   .day-data {
-    margin-bottom: 20px;
-    padding: 20px;
-    border: 1px solid #ccc;
-    background-color: #fff;
+      margin-bottom: 20px;
+      padding: 20px;
+      border: 1px solid #ccc;
+      background-color: #fff;
   }
   </style>
   
